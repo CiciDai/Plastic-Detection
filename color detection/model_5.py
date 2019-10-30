4ffb8c6362f438c0be38fe14d4dc0a7efd9c66c4
CiciDai <46978401+CiciDai@users.noreply.github.com>
2019-10-30 13:32:07 -0700

    Add files via upload

--- color detection/model_5.py
+++ color detection/model_5.py
@@ -1,7 +1,6 @@
 from __future__ import print_function
 import cv2
 import numpy as np
-import time
 
 def CalcRatio(box1, box2, type = 'overlap'):
     (xA, yA, wA, hA) = box1
@@ -18,7 +17,6 @@ def CalcRatio(box1, box2, type = 'overlap'):
     if type == 'iou':
         return iou
     return overlap
-
 
 
 class countPlastic:
@@ -47,12 +45,14 @@ class countPlastic:
 
         self.fh = frame.shape[0]
         self.fw = frame.shape[1]
-
-        bs_frame = frame[:, 0:int((self.bs_factor) * self.fw)+50]
-        tk_frame = frame[:, int(self.tk_factor * self.fw):]
+        # print(frame[0:10, 0:10])
+        frame_copy = frame.copy()
+        frame_hsv = cv2.cvtColor(frame_copy, cv2.COLOR_BGR2HSV)
+        bs_frame = frame_hsv[:, 0:int(self.bs_factor * self.fw)+30]
+        tk_frame = frame_hsv[:, int(self.tk_factor * self.fw):]
 
         # update backSub Model
-        fgMask = self.backSub.apply(bs_frame, learningRate = -1)
+        fgMask = self.backSub.apply(bs_frame, learningRate=-1.2)
 
         # processing fgMask
         blur = cv2.GaussianBlur(fgMask, (3, 3), 0)
@@ -60,7 +60,7 @@ class countPlastic:
         kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
         kernel_dil = np.ones((5, 5), np.uint8)
         thresh_img = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
-        dilated = cv2.dilate(thresh_img, kernel_dil, iterations = 1)
+        dilated = cv2.dilate(thresh_img, kernel_dil, iterations=1)
         # cv2.imshow("dialted", cv2.resize(dilated, (480, 480/self.fw*self.fh)))
         # cv2.waitKey(30)
 
@@ -75,34 +75,36 @@ class countPlastic:
 
             (x, y, w, h) = newbox
 
+            # do not include extra long object
+            if h > 0.5*self.fh:
+                continue
+
             # only include boxes in Interval area, when box right corner pass interval area
             if (x+w) > self.bs_factor * self.fw:
                 object_list = self.tracker.getObjects()
                 # convert x to tracker region coordinates
+                x0 = x
                 x = int(x - self.tk_factor * self.fw)
 
-                # for objects wider than Interval area, trim box
-                if (x < 0):
-                    w = w + x
-                    x = 0
-
+                # for objects wider than Interval area, do not add
+                if x < 0:
+                    continue
+                
                 # calculate artificial color area ratio
-                cropped = tk_frame[y:y+h, x:x+w]
+                cropped = frame[y:y+h, x0:x0+w]
                 mask, ratio = self.color_detection(cropped)
-                #
+
+                # if color detected is < 10% of bounding area, assume no plastic is detected
+                if ratio < 0.05:
+                    continue
+                print("ratio: ")
+                print(ratio)
+
                 # output = cv2.bitwise_and(cropped, cropped, mask=mask)
                 # cv2.imshow("output", output)
                 # cv2.waitKey(0)
-                print("ratio: ")
-                print(ratio)
-                # if color detected is < 10% of bounding area, assume no plastic is detected
-                if(ratio < 0.05):
-                    continue
-
-
-
                 # Assume when two bounding boxes overlaps > 60%, they are tracking the same object
-                if (self.isOverlapped((x, y, w, h), object_list) == False):
+                if not self.isOverlapped((x, y, w, h), object_list):
                     _ = self.tracker.add(cv2.TrackerMIL_create(), tk_frame, (x, y, w, h))
                     print("Now tracking " + str(len(self.tracker.getObjects())) + " objects")
 
@@ -110,24 +112,45 @@ class countPlastic:
 
         # update tracking model
         _, boxes = self.tracker.update(tk_frame)
+        # print(boxes)
+
+        # remove boxes due to random moving object
+        valid_box = []
+        for box in boxes:
+            if box[0] + box[2] > self.interval_factor*self.fw:
+                valid_box.append(box)
+        
+        if len(valid_box) != len(boxes):
+            self.tracker = cv2.MultiTracker_create()
+            for box in valid_box:
+                    (x, y, w, h) = box
+                    # only include bounding box not in finish line
+#                     if x+w < (self.finish_factor - self.tk_factor) * self.fw:
+                    _ = self.tracker.add(cv2.TrackerMIL_create(), tk_frame, (x, y, w, h))
 
         # count target in finish zone
         self.findUniqueObjects(self.tracker.getObjects())
 
+        # remove tracking boxes if tracking number > capacity or object exists in finish zone
         if len(self.tracker.getObjects()) > self.capacity or self.count > 4:
             curr_objects = self.tracker.getObjects()
             self.tracker = cv2.MultiTracker_create()
+
             if self.count == 0:
                 # all bounding boxes are in tracking zone
                 # take out the box that is closest to finish zone and add to result
-                points = np.array()
+                points = []
                 for box in curr_objects:
-                    points.append(box[0] + box[1])
-                points = sorted(points)
+                    points += [box[0] + box[2]]
+                points = np.array(sorted(points))
                 for box in curr_objects:
                     (x, y, w, h) = box
+                    # keep capacity - 1 objects in tracker
                     if x + w < points[self.capacity-1]:
                         _ = self.tracker.add(cv2.TrackerMIL_create(), tk_frame, (x, y, w, h))
+                self.result += points.size - len(self.tracker.getObjects())
+                print("new objects number")
+                print(len(self.tracker.getObjects()))
             else:
                 for box in curr_objects:
                     (x, y, w, h) = box
@@ -135,7 +158,7 @@ class countPlastic:
                     if x+w < (self.finish_factor - self.tk_factor) * self.fw:
                         _ = self.tracker.add(cv2.TrackerMIL_create(), tk_frame, (x, y, w, h))
                 self.count = 0
-            _, boxes = self.tracker.update(tk_frame)
+            #_, boxes = self.tracker.update(tk_frame)
 
         return self.result, boxes
 
@@ -143,14 +166,21 @@ class countPlastic:
         if image is None:
             return []
         # convert white and black pixels to blue
-        image[np.where((image > 250).all(axis=2))] = [255, 0, 0]
-        image[np.where((image < 5).all(axis=2))] = [255, 0, 0]
+        img_copy = image.copy()
+        # img_copy[np.where((img_copy > 245).all(axis=2))] = [255, 0, 0]
+        # img_copy[np.where((img_copy < 10).all(axis=2))] = [255, 0, 0]
 
-        image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        image_hsv = cv2.cvtColor(img_copy, cv2.COLOR_BGR2HSV)
         h, s, v = cv2.split(image_hsv)
+        white = s < 15
+        black = v < 15
+        img_copy[white == 1] = [255, 0, 0]
+        img_copy[black == 1] = [255, 0, 0]
 
+        image_hsv = cv2.cvtColor(img_copy, cv2.COLOR_BGR2HSV)
+        h, s, v = cv2.split(image_hsv)
         # identify glare region
-        nonSat = s < 180
+        nonSat = s < 100
         kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
         nonSat = cv2.erode(nonSat.astype(np.uint8), kernel)
         v2 = v.copy()
@@ -167,7 +197,7 @@ class countPlastic:
         # pre-process and convert image to hsv
         corrected_hsv = self.preprocess_img(image)
         # consider less saturated color as natural color
-        lower = np.array([0, 60, 0])
+        lower = np.array([0, 65, 65])
         upper = np.array([255, 255, 255])
         mask = cv2.inRange(corrected_hsv, lower, upper)
         ratio = np.sum(mask/255)/mask.size
@@ -238,34 +268,4 @@ class countPlastic:
             if (overlap > 0.3):
                 return True
         return False
-
-
-if __name__ =="__main__":
-    model = countPlastic()
-    capture = cv2.VideoCapture('test videos/test_5.5.mp4')
-    if not capture.isOpened:
-        print('Unable to open file')
-        exit(0)
-    while True:
-        time1 = time.time()
-        ret, frame = capture.read()
-        if frame is None:
-            break
-
-        fh = frame.shape[0]
-        fw = frame.shape[1]
-        ratio = 720/fw
-        resize_frame = cv2.resize(frame, (720, int(ratio*fh)))
-        count, boxes = model.update(resize_frame)
-
-        # model.color_detection(frame)
-        time2 = time.time()
-        delta = time2 - time1
-        cv2.putText(resize_frame, "FPS: " + str(round(1/delta, 2)), (10, 20),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.4,
-                    (255, 0, 0), 1,
-                    cv2.LINE_AA)
-        # display bounding box for objects in tracking area
-        model.displayResults(resize_frame, count, boxes)
-
 
